780dba5342f33ffd82c0910518c3c5b5d8b94b66
Wdankjahe <61615507+Wdankjahe@users.noreply.github.com>
2022-07-15 12:56:08 +0700

    Added "Meal" Model + Adapter for image

--- app/src/main/java/com/example/sistemaapptest/MainActivity.java
+++ app/src/main/java/com/example/sistemaapptest/MainActivity.java
@@ -3,14 +3,22 @@ package com.example.sistemaapptest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.GridView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int test;
+    GridView gridView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.meal_listing);
+        gridView = findViewById(R.id.mealgrid);
+        ArrayList<Meal> mealLists = new ArrayList<>();
+        mealLists.add(new Meal("11111","Baked salmon with fennel & tomatoes","https:\\/\\/www.themealdb.com\\/images\\/media\\/meals\\/1548772327.jpg"));
+        MealAdapter adapter = new MealAdapter(this,0,mealLists);
+        gridView.setAdapter(adapter);
 
     }
 }
